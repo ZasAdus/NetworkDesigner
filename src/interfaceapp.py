c574a0f55db2a80304fc67b383a17fd4b9400cec
Dawid Ciuba <dawidciuba.com@gmail.com>
2025-01-05 11:31:48 +0100

    Python updated, PyQt6 added

--- src/interfaceapp.py
+++ src/interfaceapp.py
@@ -1,24 +1,8 @@
-import tkinter as tk
-from tkinter import ttk
+import PyQt6
 from router import Router
 from switch import Switch
 from computer import Computer
 from database import Database
 
 class Interface:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Network Simulator")
-        self.root.geometry("800x600")
-        self.root.resizable(False, False)
-        
-        self.canvas = tk.Canvas(self.root, bg="white", width=800, height=600)
-        self.canvas.pack()
-        #Ahh shit here we go
-
-
-
-def __main__():
-    root = tk.Tk()
-    app = Interface(root)
-    root.mainloop()
+    print(PyQt6.__path__)
